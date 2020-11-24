60321add35d95a8242e1da2571636fcbdafe9307
Malin Ejdbo <malin.ejdbo@gmail.com>
2020-11-24 16:23:31 +0100

    Add behavior for Galaxies buttons and add Quasars button

--- light_festival/2/openspace-statemachine.js
+++ light_festival/2/openspace-statemachine.js
@@ -1,11 +1,22 @@
+const milkyWayDistance = 1.41941169E21; // 46 Kilo parsecs in meter
+const galaxiesDistance = 7.71419395E25; // 2.5 Giga parsecs in meter
+const quasarsDistance = 1.2342712E26;   // 4 Giga parsecs in meter
+const flightZoomSpeed = 1.5;
+
 var locationButtons = {
   title: "Platser",
   buttons: {
     'Vintergatan': () => {
       console.log("Going to Milky Way");
+      applyLinearFlight(openspace, milkyWayDistance, flightZoomSpeed);
     }, 
     'Galaxer': () => {
       console.log("Going to Galaxies");
+      applyLinearFlight(openspace, galaxiesDistance, flightZoomSpeed);
+    },
+    'Kvaser': () => {
+      console.log("Going to Quasars");
+      applyLinearFlight(openspace, quasarsDistance, flightZoomSpeed);
     }
   }
 };
