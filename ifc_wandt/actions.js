23e4d2f51765f13cbe2562af358632abfe99d032
Arohdin <adam.rohdin@outlook.com>
2024-01-29 10:15:55 +0100

    updated views

--- ifc_wandt/actions.js
+++ ifc_wandt/actions.js
@@ -7,20 +7,21 @@ data = null;
 const DEFAULT_VIEW_ISS = {
   Aim: "Earth",
   Anchor: "ISS",
-  Pitch: 0.15018101487939026,
-  Position: [-67.43899536132812,-210.65560150146484,154.36618041992188],
+  Pitch: -0.09827543949719057,
+  Position: [-125.3336181640625,-187.80538940429688,147.5579071044922],
   ReferenceFrame: "Root",
-  Up: [0.8610343166397695,-0.4496842769092524,-0.2374951718862684],
-  Yaw: 0.4313466027708896
+  Up: [0.7364435859914749,-0.6469210189378098,-0.19784852768844038],
+  Yaw: 0.4397218536969106
 }
+
 const DEFAULT_VIEW_CUPOLA = {
   Aim: "Earth",
   Anchor: "ISSCupola",
-  Pitch: 1.4679537851573818e-05,
-  Position: [-0.610015869140625,42.21971893310547,-52.29005432128906],
-  ReferenceFrame: "Root",
-  Up: [-0.17402767176931486,-0.7671659578982908,-0.6173902837756269],
-  Yaw: -3.6342431037475833e-06
+  Pitch: -0.7246472933878166,
+  Position: [0.36431884765625,-0.31256866455078125,0.0852203369140625],
+  ReferenceFrame:"Root",
+  Up: [-0.01486152791626133,0.24685437119842246,0.9689386226217975],
+  Yaw: -0.037903762746649784
 }
 
 // NOTE: Assumes 29/01/2024 13:15 - 13:35 GMT
@@ -148,11 +149,11 @@ async function setViewCupola2() {
   await setViewCupolaX({
     Aim: "Earth",
     Anchor: "ISSCupola",
-    Pitch: 0.056259879758252314,
-    Position: [42.67353820800781,-45.76618194580078,24.5262451171875],
+    Pitch: -0.6589943385976632,
+    Position: [0.3853912353515625,-0.10039520263671875,0.089111328125],
     ReferenceFrame: "Root",
-    Up: [-0.14772938232406194,-0.5706479379076281,-0.807797598758453],
-    Yaw: 0.42438210121052217
+    Up: [0.016053790169846094,0.6975060436526765,0.7163990472419496],
+    Yaw: -0.4397801368512996
   });
 }
 
@@ -217,18 +218,6 @@ async function setViewDefault() {
   await setIssViewX(DEFAULT_VIEW_ISS);
 }
 
-async function setViewIss1() {
-  await setIssViewX({
-    Aim: "Earth",
-    Anchor: "ISS",
-    Pitch: 0.2977406249963338,
-    Position: [3.8730316162109375,-66.58843231201172,81.36375427246094],
-    ReferenceFrame: "Root",
-    Up: [0.9005573176815771,-0.314439885303448,-0.3002067222773628],
-    Yaw: 0.013224683936702775
-  });
-}
-
 async function setViewIss2() {
   await setIssViewX({
     Aim: "Earth",
@@ -245,18 +234,6 @@ async function setViewIss3() {
   await setIssViewX(DEFAULT_VIEW_ISS, true);
 }
 
-async function setViewIss4() {
-  await setIssViewX({
-    Aim: "Earth",
-    Anchor: "ISS",
-    Pitch: 0.29774386644308404,
-    Position: [7.22137451171875,-156.7555923461914,185.05685424804688],
-    ReferenceFrame: "Root", 
-    Up: [0.9090562728236633,-0.2997726485055965,-0.2894011956573259],
-    Yaw: 0.013222002739655948
-  }, true);
-}
-
 async function setViewIss5() {
   await setIssViewX({
     Aim: "Earth",
@@ -266,17 +243,6 @@ async function setViewIss5() {
     ReferenceFrame: "Root",
     Up: [-0.8937417073208177,0.4414005634219824,0.07994562655972115],
     Yaw: -0.058203295061939574
-  });
-}
-async function setViewIss5b() {
-  await setIssViewX({
-    Aim: "Earth",
-    Anchor: "ISS",
-    Pitch: -0.27607411793481784,
-    Position: [-16.999267578125,-141.39923858642578,156.22555541992188],
-    ReferenceFrame: "Root",
-    Up: [-0.9120993039214057,0.34839839818267426,0.21608659359116714],
-    Yaw: -0.05753359944983886
   });
 }
 
@@ -289,18 +255,6 @@ async function setViewIss6() {
     ReferenceFrame: "Root",
     Up: [-0.8937417073208177,0.4414005634219824,0.07994562655972115],
     Yaw: -0.058203295061939574
-  }, true);
-}
-
-async function setViewIss7() {
-  await setIssViewX({
-    Aim: "Earth",
-    Anchor: "ISS",
-    Pitch: -0.31427771038708596,
-    Position: [-83.00234985351562,-212.1457748413086,209.5004119873047],
-    ReferenceFrame: "Root",
-    Up: [-0.9192342062327615,0.39231900841232914,0.03307974803399],
-    Yaw: -0.05820168968375226
   }, true);
 }
 
